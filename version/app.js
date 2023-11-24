b52dcef8a5cb7799edf6e28ed5a7cd3a903b0ea8
Aleksey Kurmanov <aleksey.kurmanov@gmail.com>
2023-11-24 17:13:47 -0500

    output package.json

--- version/app.js
+++ version/app.js
@@ -1,17 +1,29 @@
 const express = require('express');
-app = express();
+const os = require("os")
+const pjson = require('./package.json');
 
-var response;
+app = express();
 
 app.get('/', function (req, res) {
 
-    response = 'This is version 2 of the app.' + '\n';
+    var response = 
+        'This is version 2 of the app.' + '\n' +
+        'host-name is "' + os.hostname() + '"\n';
 
+    response += "<hr/>\n"
+    response += "<pre>\n"
+    response += "pjson = "
+    response += JSON.stringify(pjson, null, "  ")
+    response += "\n</pre>\n"
+    
     //send the response to the client
     res.send(response);
 
 });
 
 app.listen(8080, function () {
-  console.log('Server listening on port 8080... la-la-la');
+    console.log('Server listening on port 8080... let\'s go!');
+    console.dir(pjson);
+    console.log("------------------------------")
+    console.table(pjson);
 });
